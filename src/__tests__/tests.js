0e5ad8580b794ffa6526ea6e0e5d8e423ee93ce1
Code Chrysalis <codechrysalis@Codes-Mac-mini-2.local>
2018-04-30 18:09:21 +0900

    Add skeleton for ADD_PROJECT test
    Co-authored-by: Beau Dobbin <beau@beaunus.com>
    Co-authored-by: Mike Donnelly <sqldonnelly@gmail.com>"

---
+++ src/__tests__/tests.js
@@ -0,0 +1,15 @@
+const chai = require("chai");
+const projects = require("../projects");
+
+describe("Projects", () => {
+  describe("actions", () => {
+    describe("ADD_PROJECT", () => {
+      it("should add a project to the state's projects", () => {
+        //add a project
+        //size = size plus one
+        //includes recently added project
+      });
+    });
+  });
+  describe("reducers", () => {});
+});
