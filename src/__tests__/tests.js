c3da7d2be4d8c7377b1970cf08d2de11434918c4
Code Chrysalis <codechrysalis@Codes-Mac-mini-2.local>
2018-05-01 15:00:12 +0900

    Rename variables in test for clarity.
    Co-authored-by: Beau Dobbin <beau@beaunus.com>
    Co-authored-by: Mike Donnelly <sqldonnelly@gmail.com>"

--- src/__tests__/tests.js
+++ src/__tests__/tests.js
@@ -4,14 +4,14 @@ const projects = require("../projects");
 
 describe("Projects", () => {
   describe("actions", () => {
-    const newProject = {
+    const project = {
       name: "vscode",
       url: "git@github.com:Microsoft/vscode.git",
       buildCommand: "yarn && yarn test",
       language: "JavaScript",
       builds: [],
     };
-    const newProjectId = "5";
+    const projectId = "5";
     let previousState;
 
     beforeEach(() => {
@@ -23,62 +23,62 @@ describe("Projects", () => {
     describe("ADD_PROJECT", () => {
       it("should add a project to the state's projects", () => {
         const previousNumProjects = Object.keys(previousState.projects).length;
-        const action = projects.addProject(newProjectId, newProject);
+        const action = projects.addProject(projectId, project);
         projects.store.dispatch(action);
         const currentState = projects.store.getState();
         const currentNumProjects = Object.keys(currentState.projects).length;
         expect(currentNumProjects).to.equal(previousNumProjects + 1);
-        expect(currentState.projects[newProjectId]).to.deep.equal({
-          ...newProject,
-          id: newProjectId,
+        expect(currentState.projects[projectId]).to.deep.equal({
+          ...project,
+          id: projectId,
         });
       });
     });
     describe("EDIT_PROJECT", () => {
       it("should edit a project with changes", () => {
-        let action = projects.addProject(newProjectId, newProject);
+        let action = projects.addProject(projectId, project);
         projects.store.dispatch(action);
         const changes = {
           name: "vim",
         };
-        action = projects.editProject(newProjectId, changes);
+        action = projects.editProject(projectId, changes);
         projects.store.dispatch(action);
         const currentState = projects.store.getState();
-        expect(currentState.projects[newProjectId]).to.deep.equal({
-          ...newProject,
+        expect(currentState.projects[projectId]).to.deep.equal({
+          ...project,
           ...changes,
-          id: newProjectId,
+          id: projectId,
         });
       });
     });
     describe("DELETE_PROJECT", () => {
       it("should remove a project with the given id", () => {
-        const action = projects.addProject(newProjectId, newProject);
+        const action = projects.addProject(projectId, project);
         projects.store.dispatch(action);
         const oldState = projects.store.getState();
         const oldNumProjects = Object.keys(oldState.projects).length;
-        projects.store.dispatch(projects.deleteProject(newProjectId));
+        projects.store.dispatch(projects.deleteProject(projectId));
         const newState = projects.store.getState();
         const newNumProjects = Object.keys(newState.projects).length;
         expect(newNumProjects).to.equal(oldNumProjects - 1);
-        expect(newState.projects[newProjectId]).to.be.undefined;
+        expect(newState.projects[projectId]).to.be.undefined;
       });
     });
     describe("ADD_BUILD", () => {
       it("should add a build to a project", () => {
-        let action = projects.addProject(newProjectId, newProject);
+        let action = projects.addProject(projectId, project);
         projects.store.dispatch(action);
         const oldState = projects.store.getState();
-        const oldProject = oldState.projects[newProjectId];
+        const oldProject = oldState.projects[projectId];
         const oldNumBuilds = oldProject.builds.length;
         const build = {
           buildNumber: oldNumBuilds,
           status: "Running",
         };
-        action = projects.addBuild(newProjectId, oldNumBuilds, build);
+        action = projects.addBuild(projectId, oldNumBuilds, build);
         projects.store.dispatch(action);
-        const currentProject = projects.store.getState().projects[newProjectId];
-        const newNumBuilds = currentProject.builds.length;
+        const newProject = projects.store.getState().projects[projectId];
+        const newNumBuilds = newProject.builds.length;
         expect(newNumBuilds).to.equal(oldNumBuilds + 1);
       });
     });
