48bca1d2866c73feb6d8ea35389d7ed7d5c6f7f7
Code Chrysalis <codechrysalis@Codes-Mac-mini-2.local>
2018-05-01 10:52:47 +0900

    Encapsulate test initiation
    Co-authored-by: Beau Dobbin <beau@beaunus.com>
    Co-authored-by: Mike Donnelly <sqldonnelly@gmail.com>"

--- src/__tests__/tests.js
+++ src/__tests__/tests.js
@@ -4,28 +4,29 @@ const projects = require("../projects");
 
 describe("Projects", () => {
   describe("actions", () => {
+    let newProject;
+    let newProjectId;
+    before(() => {
+      newProject = {
+        name: "vscode",
+        url: "git@github.com:Microsoft/vscode.git",
+        buildCommand: "yarn && yarn test",
+        language: "JavaScript",
+      };
+      newProjectId = "5";
+    });
     describe("ADD_PROJECT", () => {
       it("should add a project to the state's projects", () => {
         const previousState = projects.store.getState();
         const previousNumProjects = Object.keys(previousState.projects).length;
-        //add a project
-        const projectToAdd = {
-          name: "vscode",
-          url: "git@github.com:Microsoft/vscode.git",
-          buildCommand: "yarn && yarn test",
-          language: "JavaScript",
-        };
-        const newId = "5";
-        const action = projects.addProject(newId, projectToAdd);
+        const action = projects.addProject(newProjectId, newProject);
         projects.store.dispatch(action);
-        //size = size plus one
         const currentState = projects.store.getState();
         const currentNumProjects = Object.keys(currentState.projects).length;
         expect(currentNumProjects).to.equal(previousNumProjects + 1);
-        //includes recently added project
-        expect(currentState.projects[newId]).to.deep.equal({
-          ...projectToAdd,
-          id: newId,
+        expect(currentState.projects[newProjectId]).to.deep.equal({
+          ...newProject,
+          id: newProjectId,
         });
       });
     });
